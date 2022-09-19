ea8ac68734bbdebfa83fc1862861954d063d0bde
macbookair <workglinnikov@gmail.com>
2022-09-19 16:04:47 +0300

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className={'aboba'}>
+                <img
+                    src="https://cdn-icons-png.flaticon.com/512/87/87390.png"/>
+            </header>
+            <nav className={'biba'}>
+                <div>
+                    <a> Profile </a>
+                </div>
+                <div>
+                    <a> Messages </a>
+                </div>
+                <div>
+                    <a> News </a>
+                </div>
+                <div>
+                    <a> Music </a>
+                </div>
+                <div>
+                    <a> Settings </a>
+                </div>
+            </nav>
+            <div className={'sanya'}>
+                Main content
+            </div>
+        </div>);
 }
-
 export default App;
